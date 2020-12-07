786228b134940c7fc1d3a3958bcfabe3e02726f5
Christoforos Magalios <xristoforos.magalios@gmail.com>
2020-12-07 09:23:11 +0200

    BillDTO refactor.

--- finance-manager-server/src/main/java/com/github/cmag/financemanager/dto/BillDTO.java
+++ finance-manager-server/src/main/java/com/github/cmag/financemanager/dto/BillDTO.java
@@ -8,20 +8,13 @@ import lombok.Data;
  * DTO representation of a Bill.
  */
 @Data
-public class BillDTO extends BaseDTO {
+public class BillDTO extends BillInfoDTO {
 
-  private BillCategoryDTO billCategory;
-  private String code;
-  private Date issuedOn;
-  private Date dueDate;
   private Date paidOn;
   private Date consumptionFrom;
   private Date consumptionTo;
-  private String description;
   private String notes;
-  private double amount;
   private double unpaidAmount;
-  private boolean paid;
   private boolean actualBill;
   private String imgPath;
   private String imgType;
